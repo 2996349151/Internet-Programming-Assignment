b23d9cc4dd16fc42b2c9206d9e2d4ea8e02a7723
2996349151 <2996349151@qq.com>
2025-04-14 21:40:58 +1000

    1

--- Backend/database.py
+++ Backend/database.py
@@ -10,20 +10,77 @@ class MYDB:
     # Get Products
     def get_products(self):
         with self.engine.connect() as connection:
-            result = connection.execute(text("SELECT * FROM Products"))
-            products = result.fetchall()
+            products = connection.execute(text("SELECT * FROM Products")).fetchall()
             return products
         
     # Get Orders
     def get_orders(self):
         with self.engine.connect() as connection:
-            result = connection.execute(text("SELECT * FROM Orders"))
-            orders = result.fetchall()
+            orders = connection.execute(text("SELECT * FROM Orders")).fetchall()
             return orders
         
     # Get Users
     def get_users(self):
         with self.engine.connect() as connection:
-            result = connection.execute(text("SELECT * FROM Users"))
-            users = result.fetchall()
+            users = connection.execute(text("SELECT * FROM Users")).fetchall()
             return users
+    
+    # Get User_id
+    def get_user_id(self, user_name):
+        with self.engine.connect() as connection:
+            user_id = connection.execute(text("SELECT User_id FROM Users WHERE User_name = :username"), 
+                                        {"username": user_name}).fetchone()
+            if user_id is None:
+                return None
+            return user_id[0]
+        
+    # Get Prodcut_id
+    def get_product_id(self, product_name):
+        with self.engine.connect() as connection:
+            product_id = connection.execute(text("SELECT Product_id FROM Products WHERE Product_name = :product_name"), 
+                                        {"product_name": product_name}).fetchone()
+            if product_id is None:
+                return None
+            return product_id[0]
+        
+    # Check Authentication
+    def check_authentication(self, username, password):
+        with self.engine.connect() as connection:
+            user = connection.execute(text("SELECT * FROM Users WHERE User_name = :username AND Password = :password"), 
+                                    {"username": username, "password": password}).fetchone()
+            if user is None:
+                return False
+            return True
+    
+    # Register User
+    def register_user(self, username, email, password):
+        with self.engine.connect() as connection:
+            users_email = connection.execute(text("SELECT Email FROM Users ")).fetchall()
+            for user in users_email:
+                if user[0] == email:
+                    return False
+            connection.execute(text("INSERT INTO Users (User_name, Email, Password) VALUES (:username, :email, :password)"), 
+                               {"username": username, "email": email, "password": password}
+            )
+            return True
+
+    # Show order History
+    def show_order_history(self, user_id):
+        with self.engine.connect() as connection:
+            orders = connection.execute(text("SELECT * FROM Orders WHERE User_id = :user_id"), {"user_id": user_id}).fetchall()
+            return orders
+
+    # Plase Orders
+    def place_order(self, order_unit_list, cost_list, mobile_number, recipient_name, recipient_email, street, city, state, user_id, product_id_list):
+        with self.engine.connect() as connection:
+            for i in range(len(order_unit_list)):
+                connection.execute(text("INSERT INTO Orders (Order_unit, Cost, Mobile_number, Recipient_name, Recipient_email, Street, City, State, User_id, Product_id) " \
+                                        "VALUES (:order_unit, :cost, :mobile_number, :recipient_name, :recipient_email, :street, :city, :state, :user_id, :product_id)"), 
+                                    {"order_unit": order_unit_list[i], "cost": cost_list[i], "mobile_number": mobile_number, "recipient_name": recipient_name,
+                                    "recipient_email": recipient_email, "street": street, "city": city, "state": state,"user_id": user_id,"product_id": product_id_list[i]}
+                )
+                
+                # Update product unit
+                connection.execute(text("UPDATE Products SET Product_unit = Prpduct_unit - :order_unit WHERE Product_id = :product_id"), 
+                                    {"order_unit": order_unit_list[i], "product_id": product_id_list[i]}
+                )
--- Backend/main.py
+++ Backend/main.py
@@ -1,9 +1,99 @@
 from fastapi import FastAPI
 from database import MYDB
+
 app = FastAPI()
+
 
 # Test database command
 DB = MYDB()
-print(DB.get_products())
-print(DB.get_orders())
 
+def check_Authentication(username, password):
+    return DB.check_authentication(username, password)
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+# Get products
+@app.get("/products")
+def show_products():
+    
+    products = DB.get_products()
+    products = [dict(row._mapping) for row in products]
+    return {"products": products}
+
+# Get orders
+@app.get("/orders")
+def show_orders():
+    
+    orders = DB.get_orders()
+    orders = [dict(row._mapping) for row in orders]
+    return {"orders": orders}
+
+# Get users
+@app.get("/users/")
+def show_users():
+    
+    users = DB.get_users()
+    users = [dict(row._mapping) for row in users]
+    return {"users": users}
+
+#Get user history
+@app.get("/user_history/username={user_name}&password={password}")
+def get_user_history(user_name, password):
+    if not check_Authentication(user_name, password):
+        return {"error": "Authentication failed"}
+    
+    user_id = DB.get_user_id(user_name)
+    if user_id is None:
+        return {"error": "User not found"}
+    orders = DB.show_order_history(user_id)
+    orders = [dict(row._mapping) for row in orders]
+    return {"orders": orders}
+
+# Register
+@app.get("/register/username={username}&email={email}&password={password}")
+def register_user(username: str, email: str, password: str):
+    if DB.register_user(username, email, password):
+        return {"message": "User registered successfully"}
+    else:
+        return {"error": "Email already exists"}
+    
+# Place order
+@app.get("/place_order/username={username}" \
+        "&password={password}" \
+        "&order_unit_list={order_unit_list}" \
+        "&cost_list={cost_list}" \
+        "&mobile_number={mobile_number}" \
+        "&recipient_name={recipient_name}" \
+        "&recipient_email={recipient_email}" \
+        "&street={street}" \
+        "&city={city}" \
+        "&state={state}" \
+        "&user_id={user_id}" \
+        "&product_id_list={product_id_list}")
+def place_order(username,
+                password, 
+                order_unit_list,
+                cost_list,
+                mobile_number,
+                recipient_name,
+                recipient_email,
+                street,
+                city,
+                state,
+                product_id_list):
+    if not check_Authentication(username, password):
+        return {"error": "Authentication failed"}
+    
+    order_unit_list = order_unit_list.split(",")
+    order_unit_list = [int(i) for i in order_unit_list]
+    cost_list = cost_list.split(",")
+    cost_list = [float(i) for i in cost_list]
+    product_id_list = product_id_list.split(",")
+    product_id_list = [int(i) for i in product_id_list]
+    user_id = DB.get_user_id(username)
+
+    DB.place_order(order_unit_list, cost_list, mobile_number, recipient_name, recipient_email, street, city, state, user_id, product_id_list)
+    return {"message": "Order placed successfully"}
